f3c26e8c4c83382546d811e0f8a262b23c1638fc
Karsten Silkenbäumer <993392+kassi@users.noreply.github.com>
2024-05-17 14:59:29 +0200

    refactor: remove unused files

--- spec/fastlane/plugin/semantic_versioning/actions/semantic_versioning_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::SemanticVersioningAction do
-  describe "#run" do
-    it "prints a message" do
-      expect(Fastlane::UI).to receive(:message).with("The semantic_versioning plugin is working!")
-
-      Fastlane::Actions::SemanticVersioningAction.run(nil)
-    end
-  end
-end
